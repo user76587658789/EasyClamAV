81f4e5410f0d15f20713365cc8ea650aabf0b2c3
user76587658789 <105585239+user76587658789@users.noreply.github.com>
2022-05-21 02:47:04 +0000

    Update genscripts.sh

--- genscripts.sh
+++ genscripts.sh
@@ -29,18 +29,30 @@ DVARP='$DVAR'
 TAILP='$(tail '$HOME'/.ClamAV/Logs/'$SNAME-$SVARP'|grep Infected|cut -d" " -f3)'
 echo "↓Enter the path to scan.↓"
 read SCANDIR
+echo "↓Update local signatures before scanning?(Yes/*No)↓"
+read SIGUP
+case "$SIGUP" in
+    [yY][eE][sS]|[yY]) 
+        SIGB="freshclam --user=$(whoami) --datadir=$PWD/Sig --config-file=$PWD/localupdate.conf"
+        ;;
+    *)
+        SIGB=""
+        ;;
+esac
 echo "↓Turn on GUI notification?(*Yes/No)↓"
 read NREAD
 case "$NREAD" in
 [yY][eE][sS]|[yY]) 
 cat << EOT > $PWD/Schedule/$SNAME.sh
 #!/bin/bash
+$SIGB
 notify-send $SNAME "ClamAV is scanning..." && SVAR=$DATEP;/usr/bin/clamscan -l $HOME/.ClamAV/Logs/$SNAME-$SVARP $SETP $SCANDIR 2>/dev/null;DVAR=$TAILP;notify-send "Done, Infected:"$DVARP
 EOT
 ;;
 *)
 cat << EOT > $PWD/Schedule/$SNAME.sh
 #!/bin/bash
+$SIGB
 /usr/bin/clamscan -l $HOME/.ClamAV/Logs/$SNAME-$DATEP $SETP $SCANDIR 2>/dev/null
 EOT
 ;;

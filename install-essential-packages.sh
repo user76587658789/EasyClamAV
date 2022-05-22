7f51c57c69b26257f1ff400429931d7024a9b3ee
user76587658789 <105585239+user76587658789@users.noreply.github.com>
2022-05-22 02:11:08 +0000

    Update install-essential-packages.sh

--- install-essential-packages.sh
+++ install-essential-packages.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 echo "-----"
+echo 0: Reutn
+echo 1: Fedora Workstation - Need SU Permission
+echo 2: Fedora Silverblue - Need Reboot
+echo 3: Debian - Need SU Permission
+echo
+echo ↓Excute command↓
+read CVAR
+echo
+case $CVAR in
+[1])
 echo Install ClamAV...
 sudo dnf install clamav clamav-update
 echo Install Cron...
 sudo dnf install cronie
 exec sh $PWD/main.sh
+;;
+[2])
+echo Install ClamAV...
+rpm-ostree install clamav clamav-update cronie
+exec sh $PWD/main.sh
+;;
+[3])
+echo Install ClamAV...
+sudo apt-get install clamav
+echo Install Cron...
+sudo apt-get install cron
+exec sh $PWD/main.sh
+;;
+*)
+echo Retun to main menu.
+exec sh $PWD/main.sh
+;;
+esac
